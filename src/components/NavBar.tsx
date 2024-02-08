37235ab6c8dc470d56f69340ba64a5f9c1c31a93
Vimal Vs <vimalchan24@gmail.com>
2024-02-08 19:15:31 +0530

    title renamed

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,7 +9,7 @@ export default function NavBar() {
 
     return (
         <nav className="flex justify-between items-center w-full px-8 p-4">
-            <h1 className="text-2xl font-bold bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent">RemindMe</h1>
+            <h1 className="text-2xl font-bold bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent">TaskTrove</h1>
             <div className="flex gap-4 items-center">
                 <ThemeSwitcher />
                 {user && isLoaded ? (
