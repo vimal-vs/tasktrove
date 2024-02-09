7363c5b55675e57a2f861bc1387a0247a5613df6
Vimal Vs <vimalchan24@gmail.com>
2024-02-09 22:01:37 +0530

    import error

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -7,7 +7,7 @@ import useEmblaCarousel, {
 } from "embla-carousel-react"
 
 import { cn } from "@/src/lib/utils"
-import { Button } from "@/components/ui/button"
+import { Button } from "../../components/ui/button"
 
 type CarouselApi = UseEmblaCarouselType[1]
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>
