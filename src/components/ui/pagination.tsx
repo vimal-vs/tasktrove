da9a063dd545805a68db2495097953c1407db895
Vimal Vs <vimalchan24@gmail.com>
2024-02-09 22:04:10 +0530

    import errors resolved

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -6,7 +6,7 @@ import {
 } from "@radix-ui/react-icons"
 
 import { cn } from "@/src/lib/utils"
-import { ButtonProps, buttonVariants } from "@/components/ui/button"
+import { ButtonProps, buttonVariants } from "../../components/ui/button"
 
 const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
   <nav
