51963952aa59edcecc6ad6192a7f46bbcea6bb3d
Vimal Vs <vimalchan24@gmail.com>
2024-02-13 22:25:34 +0530

    react-email fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     trailingSlash: false,
+    experimental: {
+        serverComponentsExternalPackages: [
+            '@react-email/components',
+            '@react-email/render',
+            '@react-email/tailwind'
+        ]
+    }
 }
 
 module.exports = nextConfig
